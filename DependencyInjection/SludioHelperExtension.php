cf8dc480323bade8f1cb8ec75721790658d6786a
Dāvis Zālītis <davis.zalitis@gmail.com>
2016-12-08 15:32:17 +0200

    config redo

--- DependencyInjection/SludioHelperExtension.php
+++ DependencyInjection/SludioHelperExtension.php
@@ -25,12 +25,33 @@ class SludioHelperExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-
+        
         $container->setParameter('sludio_helper.locales', $config['locales']);
         $container->setParameter('sludio_helper.template', $config['template']);
-
-        $container->setParameter('sludio_helper.position.position.field', $config['position_field']);
-        $positionHandler = 'sludio_helper.position.orm';
-        $container->setAlias('sludio_helper.position', new Alias($positionHandler));
+        
+        if($config['extensions']['translatable']){
+            $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Translatable/Resources/config'));
+            $loader->load('services.yml');
+        }
+        if($config['extensions']['missing']){
+            $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Missing/Resources/config'));
+            $loader->load('services.yml');
+        }
+        if($config['extensions']['beautify']){
+            $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Beautify/Resources/config'));
+            $loader->load('services.yml');
+        }
+        if($config['extensions']['position']){
+            $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Position/Resources/config'));
+            $loader->load('services.yml');
+            
+            $container->setParameter('sludio_helper.position.position.field', $config['position_field']);
+            $positionHandler = 'sludio_helper.position.orm';
+            $container->setAlias('sludio_helper.position', new Alias($positionHandler));
+        }
+        if($config['extensions']['browser']){
+            $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Browser/Resources/config'));
+            $loader->load('services.yml');
+        }
     }
 }
