d5383a1fab39e90f0d6fb05f0aefe742412318a3
Dāvis Zālītis <davis.zalitis@gmail.com>
2017-06-01 14:20:54 +0300

    OAuth config redo

---
+++ DependencyInjection/Extension/BaseExtension.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Sludio\HelperBundle\DependencyInjection\Extension;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\DependencyInjection\Reference;
+
+use Sludio\HelperBundle\DependencyInjection\Providers\CustomProviderConfigurator;
+
+class BaseExtension extends Extension
+{
+    protected $checkExternalClassExistence;
+
+    protected $configurators = [];
+
+    protected static $supportedProviderTypes = [
+        'custom' => CustomProviderConfigurator::class,
+    ];
+
+    public function __construct($checkExternalClassExistence = true)
+    {
+        $this->checkExternalClassExistence = $checkExternalClassExistence;
+    }
+
+    public static function getAllSupportedTypes()
+    {
+        return array_keys(self::$supportedProviderTypes);
+    }
+
+    public function getConfigurator($type)
+    {
+        if (!isset($this->configurators[$type])) {
+            $class = self::$supportedProviderTypes[$type];
+
+            $this->configurators[$type] = new $class();
+        }
+
+        return $this->configurators[$type];
+    }
+
+    private function buildConfigurationForType(NodeDefinition $node, $type)
+    {
+        $optionsNode = $node->children();
+        $optionsNode
+            ->scalarNode('client_id')->isRequired()->end()
+            ->scalarNode('client_secret')->isRequired()->end()
+            ->booleanNode('use_state')->defaultValue(true)->end()
+        ;
+
+        $this->getConfigurator($type)
+            ->buildConfiguration($optionsNode);
+        $optionsNode->end();
+    }
+
+    private function configureProviderAndClient(ContainerBuilder $container, $providerType, $providerKey, $providerClass, $clientClass, $packageName, array $options, $useState)
+    {
+        if ($this->checkExternalClassExistence && !class_exists($providerClass)) {
+            throw new \LogicException(sprintf(
+                'Run `composer require %s` in order to use the "%s" OAuth provider.',
+                $packageName,
+                $providerType
+            ));
+        }
+
+        $providerServiceKey = sprintf('sludio_helper.oauth.provider.%s', $providerKey);
+
+        $providerDefinition = $container->register(
+            $providerServiceKey,
+            $providerClass
+        );
+        $providerDefinition->setPublic(false);
+
+        $providerDefinition->setFactory([
+            new Reference('sludio_helper.oauth.provider_factory'),
+            'createProvider',
+        ]);
+
+        $providerDefinition->setArguments([
+            $providerClass,
+            $options,
+        ]);
+
+        $clientServiceKey = sprintf('sludio_helper.oauth.client.%s', $providerKey);
+        $clientDefinition = $container->register(
+            $clientServiceKey,
+            $clientClass
+        );
+        $clientDefinition->setArguments([
+            new Reference($providerServiceKey),
+            new Reference('request_stack'),
+        ]);
+
+        if (!$useState) {
+            $clientDefinition->addMethodCall('setAsStateless');
+        }
+
+        return $clientServiceKey;
+    }
+
+    public function configureOAuth(ContainerBuilder &$container){
+        $clientConfigurations = $container->getParameter('sludio_helper.oauth.clients');
+        $clientServiceKeys = [];
+        foreach ($clientConfigurations as $key => $clientConfig) {
+            if (!isset($clientConfig['type'])) {
+                throw new InvalidConfigurationException(sprintf(
+                   'Your "sludio_helper_oauth_client.clients." config entry is missing the "type" key.',
+                   $key
+               ));
+            }
+            $type = $clientConfig['type'];
+            unset($clientConfig['type']);
+            if (!isset(self::$supportedProviderTypes[$type])) {
+                throw new InvalidConfigurationException(sprintf(
+                    'The "sludio_helper_oauth_client.clients" config "type" key "%s" is not supported. We support (%s)',
+                    $type,
+                    implode(', ', self::$supportedProviderTypes)
+                ));
+            }
+            $tree = new TreeBuilder();
+            $node = $tree->root('sludio_helper_oauth_client/clients/' . $key);
+            $this->buildConfigurationForType($node, $type);
+            $processor = new Processor();
+            $config = $processor->process($tree->buildTree(), [$clientConfig]);
+
+            $configurator = $this->getConfigurator($type);
+            $clientServiceKey = $this->configureProviderAndClient(
+                $container,
+                $type,
+                $key,
+                $configurator->getProviderClass($config),
+                $configurator->getClientClass($config),
+                $configurator->getPackagistName(),
+                $configurator->getProviderOptions($config),
+                $config['use_state']
+            );
+
+            $clientServiceKeys[$key] = $clientServiceKey;
+        }
+
+        $container->getDefinition('sludio_helper.oauth.registry')->replaceArgument(1, $clientServiceKeys);
+    }
+}
--- DependencyInjection/SludioHelperExtension.php
+++ DependencyInjection/SludioHelperExtension.php
@@ -4,15 +4,8 @@ namespace Sludio\HelperBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\Config\Definition\Builder\NodeDefinition;
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
-use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
-use Symfony\Component\Config\Definition\Processor;
-use Symfony\Component\DependencyInjection\Reference;
-
-use Sludio\HelperBundle\DependencyInjection\Providers\CustomProviderConfigurator;
+use Sludio\HelperBundle\DependencyInjection\Extension\BaseExtension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -21,99 +14,11 @@ use Sludio\HelperBundle\DependencyInjection\Providers\CustomProviderConfigurator
  */
 class SludioHelperExtension extends Extension
 {
-    private $checkExternalClassExistence;
-    
-    private $configurators = [];
-    
-    private static $supportedProviderTypes = [
-        'custom' => CustomProviderConfigurator::class,
-    ];
-    
-    public function __construct($checkExternalClassExistence = true)
-    {
-        $this->checkExternalClassExistence = $checkExternalClassExistence;
-    }
-    
-    public static function getAllSupportedTypes()
-    {
-        return array_keys(self::$supportedProviderTypes);
-    }
-    
-    public function getConfigurator($type)
-    {
-        if (!isset($this->configurators[$type])) {
-            $class = self::$supportedProviderTypes[$type];
-
-            $this->configurators[$type] = new $class();
-        }
-
-        return $this->configurators[$type];
-    }
-    
-    private function buildConfigurationForType(NodeDefinition $node, $type)
-    {
-        $optionsNode = $node->children();
-        $optionsNode
-            ->scalarNode('client_id')->isRequired()->end()
-            ->scalarNode('client_secret')->isRequired()->end()
-            ->booleanNode('use_state')->defaultValue(true)->end()
-        ;
-
-        $this->getConfigurator($type)
-            ->buildConfiguration($optionsNode);
-        $optionsNode->end();
-    }
-    
     public function getAlias()
     {
         return 'sludio_helper';
     }
-    
-    private function configureProviderAndClient(ContainerBuilder $container, $providerType, $providerKey, $providerClass, $clientClass, $packageName, array $options, $useState)
-    {
-        if ($this->checkExternalClassExistence && !class_exists($providerClass)) {
-            throw new \LogicException(sprintf(
-                'Run `composer require %s` in order to use the "%s" OAuth provider.',
-                $packageName,
-                $providerType
-            ));
-        }
 
-        $providerServiceKey = sprintf('sludio_helper.oauth.provider.%s', $providerKey);
-
-        $providerDefinition = $container->register(
-            $providerServiceKey,
-            $providerClass
-        );
-        $providerDefinition->setPublic(false);
-
-        $providerDefinition->setFactory([
-            new Reference('sludio_helper.oauth.provider_factory'),
-            'createProvider',
-        ]);
-
-        $providerDefinition->setArguments([
-            $providerClass,
-            $options,
-        ]);
-
-        $clientServiceKey = sprintf('sludio_helper.oauth.client.%s', $providerKey);
-        $clientDefinition = $container->register(
-            $clientServiceKey,
-            $clientClass
-        );
-        $clientDefinition->setArguments([
-            new Reference($providerServiceKey),
-            new Reference('request_stack'),
-        ]);
-
-        if (!$useState) {
-            $clientDefinition->addMethodCall('setAsStateless');
-        }
-
-        return $clientServiceKey;
-    }
-    
     /**
      * {@inheritdoc}
      */
@@ -150,48 +55,9 @@ class SludioHelperExtension extends Extension
                 $container->setParameter('sludio_helper.'.$key.'.'.$var, $config['other'][$key][$var]);
             }
         }
-        
-        if ($container->hasParameter('sludio_helper.oauth.enabled')) {
-            $clientConfigurations = $container->getParameter('sludio_helper.oauth.clients');
-            $clientServiceKeys = [];
-            foreach ($clientConfigurations as $key => $clientConfig) {
-                if (!isset($clientConfig['type'])) {
-                    throw new InvalidConfigurationException(sprintf(
-                       'Your "sludio_helper_oauth_client.clients." config entry is missing the "type" key.',
-                       $key
-                   ));
-                }
-                $type = $clientConfig['type'];
-                unset($clientConfig['type']);
-                if (!isset(self::$supportedProviderTypes[$type])) {
-                    throw new InvalidConfigurationException(sprintf(
-                        'The "sludio_helper_oauth_client.clients" config "type" key "%s" is not supported. We support (%s)',
-                        $type,
-                        implode(', ', self::$supportedProviderTypes)
-                    ));
-                }
-                $tree = new TreeBuilder();
-                $node = $tree->root('sludio_helper_oauth_client/clients/' . $key);
-                $this->buildConfigurationForType($node, $type);
-                $processor = new Processor();
-                $config = $processor->process($tree->buildTree(), [$clientConfig]);
 
-                $configurator = $this->getConfigurator($type);
-                $clientServiceKey = $this->configureProviderAndClient(
-                    $container,
-                    $type,
-                    $key,
-                    $configurator->getProviderClass($config),
-                    $configurator->getClientClass($config),
-                    $configurator->getPackagistName(),
-                    $configurator->getProviderOptions($config),
-                    $config['use_state']
-                );
-                
-                $clientServiceKeys[$key] = $clientServiceKey;
-            }
-            
-            $container->getDefinition('sludio_helper.oauth.registry')->replaceArgument(1, $clientServiceKeys);
+        if ($container->hasParameter('sludio_helper.oauth.enabled')) {
+            $this->configureOAuth($container);
         }
     }
 }
