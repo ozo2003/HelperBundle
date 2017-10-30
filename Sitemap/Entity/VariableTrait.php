803693d1851429e4f9c5b81714ba02568a591373
Unknown <davis.zalitis@gmail.com>
2017-10-30 22:27:03 +0200

    fixes_0032

---
+++ Sitemap/Entity/VariableTrait.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Sludio\HelperBundle\Sitemap\Entity;
+
+trait VariableTrait
+{
+    protected $thumbnailLoc = null;
+    protected $title = null;
+    protected $description = null;
+    protected $contentLoc = null;
+    protected $playerLoc = null;
+    protected $duration = null;
+    protected $expirationDate = null;
+    protected $rating = null;
+    protected $viewCount = null;
+    protected $publicationDate = null;
+    protected $familyFriendly = null;
+    protected $tags = [];
+    protected $category = null;
+    protected $restrictions = null;
+    protected $galleryLoc = null;
+    protected $requiresSubscription = null;
+    protected $uploader = null;
+    protected $platforms = null;
+    protected $live = null;
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setThumbnailLoc($loc)
+    {
+        $this->thumbnailLoc = $loc;
+
+        return $this;
+    }
+
+    public function getThumbnailLoc()
+    {
+        return $this->thumbnailLoc;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setContentLoc($loc)
+    {
+        $this->contentLoc = $loc;
+
+        return $this;
+    }
+
+    public function getContentLoc()
+    {
+        return $this->contentLoc;
+    }
+
+    public function getPlayerLoc()
+    {
+        return $this->playerLoc;
+    }
+
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    public function getViewCount()
+    {
+        return $this->viewCount;
+    }
+
+    public function setFamilyFriendly($friendly)
+    {
+        $this->familyFriendly = (bool)$friendly;
+
+        return $this;
+    }
+
+    public function getFamilyFriendly()
+    {
+        return $this->familyFriendly;
+    }
+
+    public function getTags()
+    {
+        return $this->tags;
+    }
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    public function getRestrictions()
+    {
+        return $this->restrictions;
+    }
+
+    public function getGalleryLoc()
+    {
+        return $this->galleryLoc;
+    }
+
+    public function setRequiresSubscription($requiresSubscription)
+    {
+        $this->requiresSubscription = (bool)$requiresSubscription;
+
+        return $this;
+    }
+
+    public function getRequiresSubscription()
+    {
+        return $this->requiresSubscription;
+    }
+
+    public function getUploader()
+    {
+        return $this->uploader;
+    }
+
+    public function getPlatforms()
+    {
+        return $this->platforms;
+    }
+
+    public function setLive($live)
+    {
+        $this->live = (bool)$live;
+
+        return $this;
+    }
+
+    public function getLive()
+    {
+        return $this->live;
+    }
+}
--- Sitemap/Entity/Video.php
+++ Sitemap/Entity/Video.php
@@ -4,32 +4,14 @@ namespace Sludio\HelperBundle\Sitemap\Entity;
 
 class Video
 {
+    use VariableTrait;
+
     const RESTRICTION_DENY = 'deny';
     const RESTRICTION_ALLOW = 'allow';
 
     const PLATFORM_TV = 'tv';
     const PLATFORM_MOBILE = 'mobile';
     const PLATFORM_WEB = 'web';
-
-    protected $thumbnailLoc = null;
-    protected $title = null;
-    protected $description = null;
-    protected $contentLoc = null;
-    protected $playerLoc = null;
-    protected $duration = null;
-    protected $expirationDate = null;
-    protected $rating = null;
-    protected $viewCount = null;
-    protected $publicationDate = null;
-    protected $familyFriendly = null;
-    protected $tags = [];
-    protected $category = null;
-    protected $restrictions = null;
-    protected $galleryLoc = null;
-    protected $requiresSubscription = null;
-    protected $uploader = null;
-    protected $platforms = null;
-    protected $live = null;
 
     public function setTitle($title)
     {
@@ -40,23 +22,6 @@ class Video
         $this->title = $title;
 
         return $this;
-    }
-
-    public function getTitle()
-    {
-        return $this->title;
-    }
-
-    public function setThumbnailLoc($loc)
-    {
-        $this->thumbnailLoc = $loc;
-
-        return $this;
-    }
-
-    public function getThumbnailLoc()
-    {
-        return $this->thumbnailLoc;
     }
 
     public function setDescription($description)
@@ -70,43 +35,19 @@ class Video
         return $this;
     }
 
-    public function getDescription()
-    {
-        return $this->description;
-    }
-
-    public function setContentLoc($loc)
-    {
-        $this->contentLoc = $loc;
-
-        return $this;
-    }
-
-    public function getContentLoc()
-    {
-        return $this->contentLoc;
-    }
-
     public function setPlayerLoc($loc, $allowEmbed = true, $autoplay = null)
     {
         if ($loc === null) {
             $this->playerLoc = null;
-
-            return $this;
+        } else {
+            $this->playerLoc = [
+                'loc' => $loc,
+                'allow_embed' => $allowEmbed,
+                'autoplay' => $autoplay !== null ? $autoplay : null,
+            ];
         }
 
-        $this->playerLoc = [
-            'loc' => $loc,
-            'allow_embed' => $allowEmbed,
-            'autoplay' => $autoplay !== null ? $autoplay : null,
-        ];
-
         return $this;
-    }
-
-    public function getPlayerLoc()
-    {
-        return $this->playerLoc;
     }
 
     public function setDuration($duration)
@@ -120,11 +61,6 @@ class Video
         $this->duration = $duration;
 
         return $this;
-    }
-
-    public function getDuration()
-    {
-        return $this->duration;
     }
 
     public function setExpirationDate($date)
@@ -160,27 +96,15 @@ class Video
         return $this;
     }
 
-    public function getRating()
-    {
-        return $this->rating;
-    }
-
     public function setViewCount($count)
     {
-        $count = (int)$count;
-
-        if ($count < 0) {
+        if (intval($count) < 0) {
             throw new \DomainException('The view count must be positive');
         }
 
         $this->viewCount = $count;
 
         return $this;
-    }
-
-    public function getViewCount()
-    {
-        return $this->viewCount;
     }
 
     public function setPublicationDate($date)
@@ -203,38 +127,19 @@ class Video
         return $this->publicationDate->format(\DateTime::W3C);
     }
 
-    public function setFamilyFriendly($friendly)
-    {
-        $this->familyFriendly = (bool)$friendly;
-
-        return $this;
-    }
-
-    public function getFamilyFriendly()
-    {
-        return $this->familyFriendly;
-    }
-
     public function setTags($tags)
     {
         if ($tags === null) {
             $this->tags = null;
+        } else {
+            if (count($tags) > 32) {
+                throw new \DomainException('A maximum of 32 tags is allowed.');
+            }
 
-            return $this;
+            $this->tags = $tags;
         }
-
-        if (count($tags) > 32) {
-            throw new \DomainException('A maximum of 32 tags is allowed.');
-        }
-
-        $this->tags = $tags;
 
         return $this;
-    }
-
-    public function getTags()
-    {
-        return $this->tags;
     }
 
     public function setCategory($category)
@@ -248,132 +153,75 @@ class Video
         return $this;
     }
 
-    public function getCategory()
-    {
-        return $this->category;
-    }
-
     public function setRestrictions($restrictions, $relationship = self::RESTRICTION_DENY)
     {
         if ($restrictions === null) {
             $this->restrictions = null;
+        } else {
+            if ($relationship !== self::RESTRICTION_ALLOW && $relationship !== self::RESTRICTION_DENY) {
+                throw new \InvalidArgumentException('The relationship must be deny or allow');
+            }
 
-            return $this;
+            $this->restrictions = [
+                'countries' => $restrictions,
+                'relationship' => $relationship,
+            ];
         }
-
-        if ($relationship !== self::RESTRICTION_ALLOW && $relationship !== self::RESTRICTION_DENY) {
-            throw new \InvalidArgumentException('The relationship must be deny or allow');
-        }
-
-        $this->restrictions = [
-            'countries' => $restrictions,
-            'relationship' => $relationship,
-        ];
 
         return $this;
-    }
-
-    public function getRestrictions()
-    {
-        return $this->restrictions;
     }
 
     public function setGalleryLoc($loc, $title = null)
     {
         if ($loc === null) {
             $this->galleryLoc = null;
-
-            return $this;
+        } else {
+            $this->galleryLoc = [
+                'loc' => $loc,
+                'title' => $title,
+            ];
         }
 
-        $this->galleryLoc = [
-            'loc' => $loc,
-            'title' => $title,
-        ];
-
         return $this;
-    }
-
-    public function getGalleryLoc()
-    {
-        return $this->galleryLoc;
-    }
-
-    public function setRequiresSubscription($requiresSubscription)
-    {
-        $this->requiresSubscription = (bool)$requiresSubscription;
-
-        return $this;
-    }
-
-    public function getRequiresSubscription()
-    {
-        return $this->requiresSubscription;
     }
 
     public function setUploader($uploader, $info = null)
     {
         if ($uploader === null) {
             $this->uploader = null;
-
-            return $this;
+        } else {
+            $this->uploader = [
+                'name' => $uploader,
+                'info' => $info,
+            ];
         }
 
-        $this->uploader = [
-            'name' => $uploader,
-            'info' => $info,
-        ];
-
         return $this;
-    }
-
-    public function getUploader()
-    {
-        return $this->uploader;
     }
 
     public function setPlatforms($platforms)
     {
         if ($platforms === null) {
             $this->platforms = null;
+        } else {
+            $valid_platforms = [
+                self::PLATFORM_TV,
+                self::PLATFORM_WEB,
+                self::PLATFORM_MOBILE,
+            ];
+            foreach ($platforms as $platform => $relationship) {
+                if (!in_array($platform, $valid_platforms)) {
+                    throw new \DomainException(sprintf('Invalid platform given. Valid values are: %s', implode(', ', $valid_platforms)));
+                }
 
-            return $this;
-        }
-
-        $valid_platforms = [
-            self::PLATFORM_TV,
-            self::PLATFORM_WEB,
-            self::PLATFORM_MOBILE,
-        ];
-        foreach ($platforms as $platform => $relationship) {
-            if (!in_array($platform, $valid_platforms)) {
-                throw new \DomainException(sprintf('Invalid platform given. Valid values are: %s', implode(', ', $valid_platforms)));
+                if ($relationship !== self::RESTRICTION_ALLOW && $relationship !== self::RESTRICTION_DENY) {
+                    throw new \InvalidArgumentException('The relationship must be deny or allow');
+                }
             }
 
-            if ($relationship !== self::RESTRICTION_ALLOW && $relationship !== self::RESTRICTION_DENY) {
-                throw new \InvalidArgumentException('The relationship must be deny or allow');
-            }
+            $this->platforms = $platforms;
         }
 
-        $this->platforms = $platforms;
-
         return $this;
-    }
-
-    public function getPlatforms()
-    {
-        return $this->platforms;
-    }
-
-    public function setLive($live)
-    {
-        $this->live = (bool)$live;
-
-        return $this;
-    }
-
-    public function getLive()
-    {
-        return $this->live;
     }
 }
