ad0a3263bc7cc37df4b8857d7d860095ae83e311
Dāvis Zālītis <davis.zalitis@lattelecom.lv>
2018-01-11 19:52:24 +0200

    WIP requirements cleanup

--- DependencyInjection/Requirement/Script.php
+++ DependencyInjection/Requirement/Script.php
@@ -2,7 +2,20 @@
 
 namespace Sludio\HelperBundle\DependencyInjection\Requirement;
 
-class Script
-{
+use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
+use Mobile_Detect;
 
+class Script extends AbstractRequirement
+{
+    protected $requirements = [
+        ResponseInterface::class => 'psr/http-message',
+        LoggerInterface::class => 'psr/log',
+        Mobile_Detect::class => 'mobiledetect/mobiledetectlib'
+    ];
+
+    public function getRequirements()
+    {
+        return $this->requirements;
+    }
 }
