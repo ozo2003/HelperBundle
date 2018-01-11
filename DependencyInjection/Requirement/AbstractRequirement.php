80df18e7b07d386a20e8dbaac192f29a957e4039
Dāvis Zālītis <davis.zalitis@lattelecom.lv>
2018-01-11 19:40:46 +0200

    WIP requirements cleanup

--- DependencyInjection/Requirement/AbstractRequirement.php
+++ DependencyInjection/Requirement/AbstractRequirement.php
@@ -2,12 +2,14 @@
 
 namespace Sludio\HelperBundle\DependencyInjection\Requirement;
 
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+
 abstract class AbstractRequirement
 {
     public function check()
     {
         foreach ($this->getRequirements() as $class => $requirement) {
-            if (!class_exists($class)) {
+            if (!class_exists($class) && !interface_exists($class)) {
                 throw new InvalidConfigurationException($requirement);
             }
         }
