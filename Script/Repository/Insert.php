c703ee1c700b00e27714ee14eff3d81880710e75
Dāvis Zālītis <davis.zalitis@lattelecom.lv>
2018-04-27 16:30:56 +0300

    quick fix

--- Script/Repository/Insert.php
+++ Script/Repository/Insert.php
@@ -5,21 +5,21 @@ namespace Sludio\HelperBundle\Script\Repository;
 use Doctrine\Common\Persistence\Mapping\ClassMetadata;
 use Sludio\HelperBundle\Script\Utils\Helper;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
 class Insert
 {
-    protected $manager;
+    use ContainerAwareTrait;
 
+    protected $manager;
     protected $connection;
 
     /**
      * Insert constructor.
      *
-     * @param ContainerInterface                 $container
      * @param string|null|EntityManagerInterface $manager
      */
-    public function __construct(ContainerInterface $container, $manager = null)
+    public function __construct($manager = null)
     {
 
     }
